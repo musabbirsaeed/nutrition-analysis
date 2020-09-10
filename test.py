a75b5f28baf7113c8674c4f78deec32abd3f78e6
musabbirsaeed <m.a.saeed@gmail.com>
2020-09-10 17:35:26 -0400

    my first

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-import flask
+from flask import Flask
 
-print ("hello world")
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1> Hello World! </h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
